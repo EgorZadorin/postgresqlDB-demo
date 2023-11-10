48c487a79f0de25159e0bfdf118e0de47539bcbe
EgorZadorin <egorzadorin04@gmail.com>
2023-11-10 15:46:25 +0100

    refactoring

--- src/main/java/com/example/postgresql/repository/TutorialRepository.java
+++ src/main/java/com/example/postgresql/repository/TutorialRepository.java
@@ -3,7 +3,9 @@ package com.example.postgresql.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.postgresql.model.Tutorial;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
     List<Tutorial> findByTitleContaining(String title);
